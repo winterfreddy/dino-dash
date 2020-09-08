638b2d6872bf7cefa9c82747179aa3fb1d637fd6
Winfred Huang <huang.winfred21@gmail.com>
2020-09-08 15:06:42 -0700

    Implemented tabbed sidebar

---
+++ src/sidebar.js
@@ -0,0 +1,15 @@
+function openTab(e, tabName) {
+
+    let tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    let tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    e.currentTarget.className += " active";
+}
