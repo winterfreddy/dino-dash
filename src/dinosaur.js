2549aebeb64ac915c6d6cdadbc65b1a852b230ee
Winfred Huang <huang.winfred21@gmail.com>
2020-09-09 11:58:07 -0700

    Refactored code

--- src/dinosaur.js
+++ src/dinosaur.js
@@ -1,6 +1,3 @@
-let canvasWidth = 700;
-let canvasHeight = 500;
-
 let idleDinoCharacter = new Image();
 idleDinoCharacter.src = "../public/images/css_sprites_idle.png";
 let walkDinoCharacter = new Image();
@@ -14,12 +11,8 @@ tripDinoCharacter.src = "../public/images/css_sprites_trip.png";
 let deadDinoCharacter = new Image();
 deadDinoCharacter.src = "../public/images/css_sprites_dead.png";
 
-let dinoIdleSpriteWidth = 7000;
-let dinoWalkSpriteWidth = 7000;
-let dinoRunSpriteWidth = 5600;
-let dinoJumpSpriteWidth = 8400;
-let dinoTripSpriteWidth = 2100;
-let dinoDeadSpriteWidth = 5600;
+let canvasWidth = 700;
+let canvasHeight = 500;
 let dinoWidth = 700;
 let dinoHeight = 492;
 
@@ -37,109 +30,79 @@ let jump = false;
 let trip = false;
 let dead = false;
 
-// starting frame position
 let x = 0;
 let y = 0;
-
-// what are these?
 let srcX;
 let srcY;
 
 let currentFrame = 0;
 
 let canvas = document.getElementById("myCanvas");
-console.log(canvas);
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 let ctx = canvas.getContext('2d');
 
 function updateFrame() {
     ctx.clearRect(x, y, dinoWidth, dinoHeight);
+    let frameLength;
     if(walk) {
-        currentFrame = ++currentFrame % walkFrameLength;
+        frameLength = walkFrameLength;
     } else if(run) {
-        currentFrame = ++currentFrame % runFrameLength;
+        frameLength = runFrameLength;
     } else if(jump) {
-        currentFrame = ++currentFrame % jumpFrameLength;
+        frameLength = jumpFrameLength;
     } else if(trip) {
-        currentFrame = ++currentFrame % tripFrameLength;
+        frameLength = tripFrameLength;
     } else if(dead) {
-        currentFrame = ++currentFrame % deadFrameLength;
+        frameLength = deadFrameLength;
     } else {
-        currentFrame = ++currentFrame % idleFrameLength;
+        frameLength = idleFrameLength;
     }
+    currentFrame = ++currentFrame % frameLength;
     srcX = currentFrame * dinoWidth;
     srcY = 0;
 }
 
 function drawImage() {
     updateFrame();
+    let dinoCharacter;
     if(walk) {
-        ctx.drawImage(walkDinoCharacter, srcX, srcY, dinoWidth, dinoHeight, x, y, dinoWidth, dinoHeight);
+        dinoCharacter = walkDinoCharacter;
     } else if(run) {
-        ctx.drawImage(runDinoCharacter, srcX, srcY, dinoWidth, dinoHeight, x, y, dinoWidth, dinoHeight);
+        dinoCharacter = runDinoCharacter;
     } else if(jump) {
-        ctx.drawImage(jumpDinoCharacter, srcX, srcY, dinoWidth, dinoHeight, x, y, dinoWidth, dinoHeight);
+        dinoCharacter = jumpDinoCharacter;
     } else if(trip) {
-        ctx.drawImage(tripDinoCharacter, srcX, srcY, dinoWidth, dinoHeight, x, y, dinoWidth, dinoHeight);
+        dinoCharacter = tripDinoCharacter;
     } else if(dead) {
-        ctx.drawImage(deadDinoCharacter, srcX, srcY, dinoWidth, dinoHeight, x, y, dinoWidth, dinoHeight);
+        dinoCharacter = deadDinoCharacter;
     } else {
-        ctx.drawImage(idleDinoCharacter, srcX, srcY, dinoWidth, dinoHeight, x, y, dinoWidth, dinoHeight);
+        dinoCharacter = idleDinoCharacter;
     }
+    ctx.drawImage(dinoCharacter, srcX, srcY, dinoWidth, dinoHeight, x, y, dinoWidth, dinoHeight);
 }
 
-function dinoIdle() {
-    idle = true;
-    walk = false;
-    run = false;
-    jump = false;
-    trip = false;
-    dead = false;
-}
-
-function dinoWalk() {
-    idle = false;
-    walk = true;
-    run = false;
-    jump = false;
-    trip = false;
-    dead = false;
-}
-
-function dinoRun() {
-    idle = false;
-    walk = false;
-    run = true;
-    jump = false;
-    trip = false;
-    dead = false;
-}
-
-function dinoJump() {
-    idle = false;
-    walk = false;
-    run = false;
-    jump = true;
-    trip = false;
-    dead = false;
-}
-
-function dinoTrip() {
-    idle = false;
-    walk = false;
-    run = false;
-    jump = false;
-    trip = true;
-    dead = false;
-}
-function dinoDead() {
+function dinoAction(action) {
     idle = false;
     walk = false;
     run = false;
     jump = false;
     trip = false;
-    dead = true;
+    dead = false;
+
+    if(action === "i") {
+        idle = true;
+    } else if(action === "w") {
+        walk = true;
+    } else if(action === "r") {
+        run = true;
+    } else if(action === "j") {
+        jump = true;
+    } else if(action === "t") {
+        trip = true;
+    } else if(action === "d") {
+        dead = true;
+    }
 }
 
 setInterval(function(){
