4f3f952dfbfd2d963fb0ee36ca8272481dc08d56
Winfred Huang <huang.winfred21@gmail.com>
2020-09-09 16:54:46 -0700

    Basic startGame function

--- src/game.js
+++ src/game.js
@@ -1,4 +1,16 @@
 
 function startGame() {
     document.getElementById('type-input').focus();
+    dinoAction('w');
+    document.addEventListener('keydown', function(event) {
+        if(event.which === 13) {
+            let input = document.getElementById('type-input').value;
+            if(input === "fire") {
+                dinoAction('j');
+            } else {
+                dinoAction('w');
+            }
+            document.getElementById('type-input').value = '';
+        }
+    })
 }
